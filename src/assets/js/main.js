d2e1f066e0b743852d77e78b29d78889677baee1
Ziki <raynoldripper@gmail.com>
2024-07-12 18:20:58 +0300

    sync

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,6 @@
 // Import our custom CSS
 import '../scss/main.scss'
+import './swiper';
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
---
+++ src/assets/js/swiper.js
@@ -0,0 +1,21 @@
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+// init Swiper:
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
